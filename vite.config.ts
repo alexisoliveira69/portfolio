c15164c3766506a9c672660a8cd8871bfecae739
alexisoliveira69 <alexis.oliveira@jtekt.eu>
2025-05-26 12:12:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+export default defineConfig({
+  base: '/portfolio/', // Ajoute précisément cette ligne avec ton nom de dépôt
+  // autres configurations...
+})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
